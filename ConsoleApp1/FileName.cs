650eff57aee4bb4e6581b0026869a60dfb113877
sashaklymenko <sashaklymenko@SASHAKLYMENB81A>
2023-09-19 23:11:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/FileName.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class TTriangle
+    {
+        protected double sideA;
+        protected double sideB;
+        protected double sideC;
+
+        public TTriangle(double a, double b, double c)
+        {
+            if (IsValidT(a,b,c))
+            {
+                sideA = a; 
+                sideB = b; 
+                sideC = c;
+            }
+            else
+            {
+                Console.WriteLine("Трикутник не існує");
+            }
+        }
+
+        //перевірка існування
+        public bool IsValidT(double a, double b, double c)
+        {
+            return a + b > c & a + c > b & b + c > a && a > 0 && b >0 && c > 0;
+        }
+        //отримання сторін
+        public double GetSideA()
+        {
+            return sideA;
+        }
+        public double GetSideB()
+        { 
+            return sideB;
+        }
+        public double GetSideC()
+        {
+            return sideC;
+        }
+
+        public void SetSideA(double a) 
+        { 
+            if (IsValidT(a, sideB, sideC))
+            {
+                sideA = a;
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+        public void SetSideB(double b) 
+        {
+            if (IsValidT(sideA, b, sideC))
+            {
+                sideB = b;
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        public void SetSideC(double c)
+        {
+            if (IsValidT(sideA, sideB, c))
+            {
+                sideC = c;
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        //обчислення периметру
+        public double Per()
+        {
+            return sideA + sideB + sideC;
+        }
+
+        //обчислення площі Герон
+       public double Area()
+        {
+            double s = Per() / 2;
+            return Math.Sqrt(s*(s-sideA)*(s-sideB)*(s-sideC));
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            TTriangle triangle = new TTriangle(a, b, c);
+
+            Console.WriteLine("Довжина сторони A: " + triangle.GetSideA());
+            Console.WriteLine("Довжина сторони B: " + triangle.GetSideB());
+            Console.WriteLine("Довжина сторони C: " + triangle.GetSideC());
+            Console.WriteLine("Периметр трикутника: " + triangle.Per());
+            Console.WriteLine("Площа трикутника: " + triangle.Area());
+
+            double NewSideA = double.Parse(Console.ReadLine());
+            triangle.SetSideA(NewSideA);
+            Console.WriteLine("Нова довжина сторони A: " + triangle.GetSideA());
+            Console.WriteLine("Довжина сторони B: " + triangle.GetSideB());
+            Console.WriteLine("Довжина сторони C: " + triangle.GetSideC());
+            Console.WriteLine("Новий периметр трикутника: " + triangle.Per());
+            Console.WriteLine("Нова площа трикутника: " + triangle.Area());
+            Console.ReadKey();
+        }
+
+    }
+}
